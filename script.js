ef449b583e88dcf65da80226fce788178affb180
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-11 07:23:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// const uA = navigator.userAgent;
+// const aN = navigator.appName;
+// const aV = navigator.appVersion;
+
+// const browserInfo = document.getELementById("browser-info");
+// browserInfo.innerText = `You are using ${aN} version ${aV}';
